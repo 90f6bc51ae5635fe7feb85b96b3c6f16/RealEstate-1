318742fc2ba5ba4236f80fe1a1ff8a05d18330bb
Manadsawee <47310448+Manadsawee@users.noreply.github.com>
2019-02-28 16:42:09 +0700

    mix

--- view/slide.inc.php
+++ view/slide.inc.php
@@ -18,13 +18,17 @@ $slide = $slide_model -> getProductBy();
                             <h1 class="pb-4"><?php if ($lng == TH) {  echo $slide[$i]['slide_title_th'];   }else {  echo $slide[$i]['slide_title_en'];  }  ?></h1>
                             <div class="d-none d-lg-block">
                                 <h3 class="pb-4"><?php if ($lng == TH) {  echo $slide[$i]['slide_sub_title_th'];   }else {  echo $slide[$i]['slide_sub_title_en'];  }  ?></h3>
-                                
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="bg-slide-color" >
-                    <img style=" width: 100%; height: 900px; object-fit: cover;" src="img_upload/product/<?php echo $slide[$i]['product_img']; ?>">
+                    <img style=" width: 100%; height: 900px; object-fit: cover;  
+                    
+            opacity: 0;
+            filter: alpha(opacity=0); /* For IE8 and earlier */
+                    
+                    " src="img_upload/product/<?php echo $slide[$i]['product_img']; ?>">
                 </div>
             </div>
         <?php } ?>

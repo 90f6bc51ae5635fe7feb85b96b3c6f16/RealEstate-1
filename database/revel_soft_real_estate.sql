040a3816b54622ca6946edbe85ea39f5b722bcf0
chunwarayut <warayut2555@gmail.com>
2019-03-05 14:13:48 +0700

    Update revel_soft_real_estate.sql

--- database/revel_soft_real_estate.sql
+++ database/revel_soft_real_estate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 04, 2019 at 03:19 AM
+-- Generation Time: Mar 05, 2019 at 07:12 AM
 -- Server version: 5.7.17-log
 -- PHP Version: 5.6.30
 
@@ -580,6 +580,38 @@ INSERT INTO `tb_news` (`news_id`, `news_name_th`, `news_name_en`, `news_date`, `
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_page`
+--
+
+CREATE TABLE `tb_page` (
+  `page_id` int(11) NOT NULL,
+  `page_name` varchar(100) NOT NULL,
+  `page_title_th` varchar(100) DEFAULT NULL,
+  `page_title_en` varchar(100) DEFAULT NULL,
+  `page_tag_th` text,
+  `page_tag_en` text,
+  `page_description_th` text,
+  `page_description_en` text,
+  `page_image` varchar(200) NOT NULL,
+  `updateby` int(11) DEFAULT NULL,
+  `lastupdate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_page`
+--
+
+INSERT INTO `tb_page` (`page_id`, `page_name`, `page_title_th`, `page_title_en`, `page_tag_th`, `page_tag_en`, `page_description_th`, `page_description_en`, `page_image`, `updateby`, `lastupdate`) VALUES
+(1, 'index', 'REALESTATE-หน้าหลัก', 'REALESTATE-HOME PAGE ', 'หน้าหลัก', 'HOME PAGE', 'หน้าหลัก', 'HOME PAGE', '', 1, '2019-03-01 16:15:51'),
+(2, 'about', 'REALESTATE-เกี่ยวกับเรา', 'REALESTATE-ABOUT', 'REALESTATE-เกี่ยวกับเรา', 'REALESTATE-ABOUT', 'REALESTATE-เกี่ยวกับเรา', 'REALESTATE-ABOUT', '', 1, '2019-03-01 16:15:51'),
+(3, 'service', 'REALESTATE-บริการ', 'REALESTATE-SERVICE', 'REALESTATE-บริการ', 'REALESTATE-SERVICE', 'REALESTATE-บริการ', 'REALESTATE-SERVICE', '', 1, '2019-03-01 16:15:51'),
+(4, 'property', 'REALESTATE-คุณสมบัติ', 'REALESTATE-PROPERTY', 'REALESTATE-คุณสมบัติ', 'REALESTATE-PROPERTY', 'REALESTATE-คุณสมบัติ', 'REALESTATE-PROPERTY', '', 1, '2019-03-01 16:15:51'),
+(5, 'news&event', 'REALESTATE-ข่าวและเหตุการณ์', 'REALESTATE-NEWS&EVENT', 'REALESTATE-ข่าวและเหตุการณ์', 'REALESTATE-NEWS&EVENT', 'REALESTATE-ข่าวและเหตุการณ์', 'REALESTATE-NEWS&EVENT', '', 1, '2019-03-01 16:15:51'),
+(6, 'contact', 'REALESTATE-ติดต่อเรา', 'REALESTATE-CONTACT', 'REALESTATE-ติดต่อเรา', 'REALESTATE-CONTACT', 'REALESTATE-ติดต่อเรา', 'REALESTATE-CONTACT', '', 1, '2019-03-01 16:15:51');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_product`
 --
 
@@ -829,6 +861,12 @@ ALTER TABLE `tb_news`
   ADD PRIMARY KEY (`news_id`);
 
 --
+-- Indexes for table `tb_page`
+--
+ALTER TABLE `tb_page`
+  ADD PRIMARY KEY (`page_id`);
+
+--
 -- Indexes for table `tb_product`
 --
 ALTER TABLE `tb_product`
@@ -934,6 +972,11 @@ ALTER TABLE `tb_location`
 --
 ALTER TABLE `tb_news`
   MODIFY `news_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tb_page`
+--
+ALTER TABLE `tb_page`
+  MODIFY `page_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `tb_product`
 --

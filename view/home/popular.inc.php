ca75151261d7bdfae54e9b113f310a20b31ba1fa
boorapa <boorapa0147@gmail.com>
2019-03-06 16:10:37 +0700

    up

--- view/home/popular.inc.php
+++ view/home/popular.inc.php
@@ -3,21 +3,21 @@
         <legend style="width: 47.65px !important ; text-align: center;">POPPULAR PROPERTIES </legend>
         <div class="row" style="max-width: 1100px;margin: auto;">
             <div class="col-lg-6 no-padding">
-                <img class="img-fluid" src="img_upload/home/1.jpg" style="width: 35vw; margin: 5px;">
+                <img class="img-poppular-home1" src="img_upload/home/1.jpg">
             </div>
             <div class="col-lg-6 no-padding">
-                <img class="img-fluid" src="img_upload/home/1.jpg" style="width: 35vw; margin: 5px;">
+                <img class="img-poppular-home1" src="img_upload/home/1.jpg">
             </div>
         </div>
         <div class="row" style="max-width: 1100px;margin: auto;">
             <div class="col-lg-4 no-padding">
-                <img class="img-fluid" src="img_upload/home/1.jpg" style="width: 35vw; margin: 5px;">
+                <img class="img-poppular-home2" src="img_upload/home/1.jpg" >
             </div>
             <div class="col-lg-4 no-padding">
-                <img class="img-fluid" src="img_upload/home/1.jpg" style="width: 35vw; margin: 5px;">
+                <img class="img-poppular-home2" src="img_upload/home/1.jpg" >
             </div>
             <div class="col-lg-4 no-padding">
-                <img class="img-fluid" src="img_upload/home/1.jpg" style="width: 35vw; margin: 5px;">
+                <img class="img-poppular-home3" src="img_upload/home/1.jpg" >
             </div>
         </div>
     </fieldset>

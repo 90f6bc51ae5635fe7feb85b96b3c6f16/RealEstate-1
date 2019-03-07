706a4f8ba186d3873d29ae7b5f1760be2922dd4c
boorapa <boorapa0147@gmail.com>
2019-03-07 16:20:04 +0700

    up

--- view/news/view.inc.php
+++ view/news/view.inc.php
@@ -1,16 +1,14 @@
 <div class="container">
     <div class="text-left" style="padding-top: 7vw;">
-        <h1 style="color: #5a5a5a; font-weight: 600">NEWS </h1>
-        <hr class="border-news" style="border-top: 2px solid  #5a5a5a">
+        <h1 style="color: #5a5a5a; font-weight: 600">LATEST NEWS </h1>
+        <hr align="left" class="border-news" style="border-top: 2px solid  #5a5a5a; width: 7vw;">
     </div>
 
-    <div class="row service-row-box">
+    <div class="row row-news1">
         <div class="element-item col-lg-4 col-md-6 col-sm-6">
             <img src="img_upload/home/1.jpg" class="img-news">
             <div class="topic-news">
                 DDproperty Property Index Saw Prices For Residential Property In Bangkok
-
-
             </div>
             <div class="date-news">
                 Aug 12, 2015
@@ -21,11 +19,6 @@
                 as high growth areas. DDproperty revealed that demand for accommodation from
                 millennials will continue to grow significantly. In Bangkok, Chatuchak,
                 Suanluang and Phyathai are identified as high growth areas.
-            </div>
-            <div style=" padding-top:20px;  ">
-                <div class="btn btn-readmore">
-                    READ MORE
-                </div>
             </div>
         </div>
         <div class="element-item col-lg-4 col-md-6 col-sm-6">
@@ -43,6 +36,20 @@
                 millennials will continue to grow significantly. In Bangkok, Chatuchak,
                 Suanluang and Phyathai are identified as high growth areas.
             </div>
+        </div>
+        <div class="element-item col-lg-4 col-md-6 col-sm-6">
+
+        </div>
+    </div>
+    <div class="row service-row-box">
+        <div class="element-item col-lg-4 col-md-6 col-sm-6">
+            <div style=" padding-top:20px;  ">
+                <div class="btn btn-readmore">
+                    READ MORE
+                </div>
+            </div>
+        </div>
+        <div class="element-item col-lg-4 col-md-6 col-sm-6">
             <div style=" padding-top:20px;  ">
                 <div class="btn btn-readmore">
                     READ MORE
@@ -53,14 +60,11 @@
 
         </div>
     </div>
-
-    <div class="row service-row-box">
+    <div class="row row-news2">
         <div class="element-item col-lg-4 col-md-6 col-sm-6">
             <img src="img_upload/home/1.jpg" class="img-news">
             <div class="topic-news">
                 DDproperty Property Index Saw Prices For Residential Property In Bangkok
-
-
             </div>
             <div class="date-news">
                 Aug 12, 2015
@@ -71,11 +75,6 @@
                 as high growth areas. DDproperty revealed that demand for accommodation from
                 millennials will continue to grow significantly. In Bangkok, Chatuchak,
                 Suanluang and Phyathai are identified as high growth areas.
-            </div>
-            <div style=" padding-top:20px;  ">
-                <div class="btn btn-readmore">
-                    READ MORE
-                </div>
             </div>
         </div>
         <div class="element-item col-lg-4 col-md-6 col-sm-6">
@@ -93,6 +92,20 @@
                 millennials will continue to grow significantly. In Bangkok, Chatuchak,
                 Suanluang and Phyathai are identified as high growth areas.
             </div>
+        </div>
+        <div class="element-item col-lg-4 col-md-6 col-sm-6">
+
+        </div>
+    </div>
+    <div class="row service-row-box">
+        <div class="element-item col-lg-4 col-md-6 col-sm-6">
+            <div style=" padding-top:20px;  ">
+                <div class="btn btn-readmore">
+                    READ MORE
+                </div>
+            </div>
+        </div>
+        <div class="element-item col-lg-4 col-md-6 col-sm-6">
             <div style=" padding-top:20px;  ">
                 <div class="btn btn-readmore">
                     READ MORE
@@ -103,3 +116,4 @@
 
         </div>
     </div>
+</div>

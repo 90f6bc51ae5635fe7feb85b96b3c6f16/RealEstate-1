6800e1587ea751620bf9f5654c0dcfb4a1b57510
Manadsawee <47310448+Manadsawee@users.noreply.github.com>
2019-03-08 10:51:28 +0700

    mix

--- view/about/slide.inc.php
+++ view/about/slide.inc.php
@@ -1,9 +1,11 @@
 <?php require_once('view/menu.inc.php')?>
-    <section class="col-lg-12 no-padding" style="position: relative;">
+    <section class="col-lg-12 no-padding img-slide-about">
         <img class="size-img-slide" src="img_upload/home/architecture-1867187_1920.jpg">
-            <div class="overlay-slide">
+            <div class="overlay-slide-about">
             </div>
-
+            <div class="overlay2">
+            </div>
+    
     <section class="slide no-padding slide-home ">
         <div class="col-12">
             <div class="container">
@@ -13,7 +15,7 @@
                         <div class="slide-item">
                             <div class="slide-detail-home">
                                 <div class="slide-header-home">
-                                <h1 style="color: #fff; font-weight: 800;">SERVICE</h1>
+                                <h1 style="color: #fff;">ABOUT</h1>
                                 </div>
                                 <div class="slide-text-detail">
                                     Penthouse apartment for sale located on Iancu Nicolae Road. Wit New Darwin suburb
@@ -36,7 +38,7 @@
                         <div class="slide-item">
                             <div class="slide-detail-home">
                                 <div class="slide-header-home">
-                                    <h1 style="color: #fff; font-weight: 800;">SERVICE</h1>
+                                    <h1 style="color: #fff;">ABOUT</h1>
                                 </div>
                                 <div class="slide-text-detail">
                                     Penthouse apartment for sale located on Iancu Nicolae Road. Wit New Darwin suburb
@@ -57,59 +59,60 @@
 
     </section>
 
-    <div class=""
-        style="width: 100%; bottom: -8%; position: absolute; z-index: 9999; text-align: center; padding-left: 20%; padding-right: 20%;">
-        <div class="shadow-lg" style="background-color: white;">
+    <div class="box-search-home">
+            <div class="shadow-lg" style="background-color: white;">
+                <div class="row m-0 py-4">
+                    <div class="col-sm-12 col-md-3 py-md-0 py-sm-3">
+                        <div class="text-left">
+                            <div class="">
+                                <select class="form-control  custom-select" id="location_id" name="location_id">
+                                    <option value=""> Location</option>
+                                    <?PHP for ($i=0; $i < count($location); $i++) {  ?>
+                                    <option value="<?PHP echo $location[$i]['location_id'];?>" <?PHP if($_GET['location_id'] == $location[$i]['location_id']){ echo 'selected' ; } ?>>
+                                        <?PHP if($lan =='TH'){ echo $location[$i]['location_name_th']; } else { echo $location[$i]['location_name_en'];}?>
+                                    </option>
+                                    <?PHP } ?>
+                                </select>
 
-
-            <div class="row" style="padding: 1vw;">
-            <div class="element-item col-lg-3 col-md-6 col-sm-6">
-                    <div class="text-left">
-                        <div class="form-group">
-                          <br />
-                            <select class="form-control  custom-select" id="room_type_id" name="room_type_id">
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> Location</option>
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> 5</option>
-                            
-                            </select>
-                          
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="element-item col-lg-3 col-md-6 col-sm-6">
-                    <div class="text-left">
-                        <div class="form-group">
-                            <br />
-                            <select class="form-control  custom-select" id="room_type_id" name="room_type_id">
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> Property type</option>
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> 5</option>
-                                
-                            </select>
-                         
+                    <div class="col-sm-12 col-md-3 py-md-0 py-sm-3">
+                        <div class="text-left">
+                            <div class="">
+                                <select class="form-control  custom-select" id="product_types_id"
+                                    name="product_types_id">
+                                    <option value=""> Property type</option>
+                                    <?PHP for ($i=0; $i < count($type); $i++) {  ?>
+                                    <option value="<?PHP echo $type[$i]['product_types_id'];?>" <?PHP if($_GET['product_types_id'] == $type[$i]['product_types_id']){ echo 'selected' ; } ?>>
+                                        <?PHP if($lan =='TH'){ echo $type[$i]['product_types_name_th']; } else { echo $type[$i]['product_types_name_en'];}?>
+                                    </option>
+                                    <?PHP } ?>
+
+                                </select>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="element-item col-lg-3 col-md-6 col-sm-6">
-                    <div class="text-left">
-                        <div class="form-group">
-                             <br />
-                            <select class="form-control  custom-select" id="room_type_id" name="room_type_id">
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> 5</option>
-                            </select>
-                           
+                    <div class="col-sm-12 col-md-3 py-md-0 py-sm-3 ">
+                        <div class="text-left">
+                            <div class="">
+                                <div class="">
+                                    <input id="keyword" name="keyword" class="form-control" value="<?PHP echo $_GET['keyword']; ?>"  placeholder="KEYWORD" />
+                                </div>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="element-item col-lg-3 col-md-6 col-sm-6">
-                <div class="btn btn-search">
-                    SEARCH
-                </div>
+                    <div class="col-sm-3">
+                    <button onclick="search();" type="submit" class="btn btn-search">
+                            SEARCH
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-
-    </div>
 </section>
 
 <script type="text/javascript">
--- view/contact/slide.inc.php
+++ view/contact/slide.inc.php
@@ -1,27 +1,21 @@
 <?php require_once('view/menu.inc.php')?>
-<section class="col-lg-12 no-padding" style="position: relative;">
-
-
-    <img class="size-img-slide" src="img_upload/home/architecture-1477041_1280.jpg">
-    <div class="overlay-slide">
-    </div>
-    <div class="overlay-slide-2">
-    </div>
-
+    <section class="col-lg-12 no-padding img-slide-contact">
+        <img class="size-img-slide" src="img_upload/home/architecture-1477041_1280.jpg">
+            <div class="overlay-slide-contact">
+            </div>
+            <div class="overlay2">
+            </div>
+    
     <section class="slide no-padding slide-home ">
-
-
-
         <div class="col-12">
             <div class="container">
-
                 <div class="row">
                     <div class="col-7" style="margin-top: 4vw;">
 
                         <div class="slide-item">
                             <div class="slide-detail-home">
                                 <div class="slide-header-home">
-                                <h1 style="color: #fff; font-weight: 800;">CONTACT REALESTATE</h1>
+                                <h1 style="color: #fff;">CONTACT REALESTATE</h1>
                                 </div>
                                 <div class="slide-text-detail">
                                     Penthouse apartment for sale located on Iancu Nicolae Road. Wit New Darwin suburb
@@ -35,8 +29,6 @@
                 </div>
             </div>
         </div>
-
-
         <div class="col-12">
             <div class="container">
 
@@ -46,7 +38,7 @@
                         <div class="slide-item">
                             <div class="slide-detail-home">
                                 <div class="slide-header-home">
-                                <h1 style="color: #fff; font-weight: 800;">CONTACT REALESTATE</h1>
+                                    <h1 style="color: #fff;">CONTACT REALESTATE</h1>
                                 </div>
                                 <div class="slide-text-detail">
                                     Penthouse apartment for sale located on Iancu Nicolae Road. Wit New Darwin suburb
@@ -67,67 +59,61 @@
 
     </section>
 
-    <div class=""
-        style="width: 100%; bottom: -8%; position: absolute; z-index: 9999; text-align: center; padding-left: 20%; padding-right: 20%;">
-        <div class="shadow-lg" style="background-color: white;">
+    <div class="box-search-home">
+            <div class="shadow-lg" style="background-color: white;">
+                <div class="row m-0 py-4">
+                    <div class="col-sm-12 col-md-3 py-md-0 py-sm-3">
+                        <div class="text-left">
+                            <div class="">
+                                <select class="form-control  custom-select" id="location_id" name="location_id">
+                                    <option value=""> Location</option>
+                                    <?PHP for ($i=0; $i < count($location); $i++) {  ?>
+                                    <option value="<?PHP echo $location[$i]['location_id'];?>" <?PHP if($_GET['location_id'] == $location[$i]['location_id']){ echo 'selected' ; } ?>>
+                                        <?PHP if($lan =='TH'){ echo $location[$i]['location_name_th']; } else { echo $location[$i]['location_name_en'];}?>
+                                    </option>
+                                    <?PHP } ?>
+                                </select>
 
-
-            <div class="row" style="padding: 1vw;">
-                <div class="col-sm-3">
-                    <div class="text-left">
-                        <div class="form-group">
-                          <br />
-                            <select class="form-control  custom-select" id="room_type_id" name="room_type_id">
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> Location</option>
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> 5</option>
-                            
-                            </select>
-                          
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="col-sm-3">
-                    <div class="text-left">
-                        <div class="form-group">
-                            <br />
-                            <select class="form-control  custom-select" id="room_type_id" name="room_type_id">
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> Property type</option>
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> 5</option>
-                                
-                            </select>
-                         
+                    <div class="col-sm-12 col-md-3 py-md-0 py-sm-3">
+                        <div class="text-left">
+                            <div class="">
+                                <select class="form-control  custom-select" id="product_types_id"
+                                    name="product_types_id">
+                                    <option value=""> Property type</option>
+                                    <?PHP for ($i=0; $i < count($type); $i++) {  ?>
+                                    <option value="<?PHP echo $type[$i]['product_types_id'];?>" <?PHP if($_GET['product_types_id'] == $type[$i]['product_types_id']){ echo 'selected' ; } ?>>
+                                        <?PHP if($lan =='TH'){ echo $type[$i]['product_types_name_th']; } else { echo $type[$i]['product_types_name_en'];}?>
+                                    </option>
+                                    <?PHP } ?>
+
+                                </select>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-sm-3">
-                    <div class="text-left">
-                        <div class="form-group">
-                             <br />
-                            <select class="form-control  custom-select" id="room_type_id" name="room_type_id">
-                                <option value="<?PHP echo $room_type[$i]['room_type_id'];?>"> 5</option>
-                            </select>
-                           
+                    <div class="col-sm-12 col-md-3 py-md-0 py-sm-3 ">
+                        <div class="text-left">
+                            <div class="">
+                                <div class="">
+                                    <input id="keyword" name="keyword" class="form-control" value="<?PHP echo $_GET['keyword']; ?>"  placeholder="KEYWORD" />
+                                </div>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-sm-3">
-                <div class="btn btn-search">
-                    SEARCH
-                </div>
+                    <div class="col-sm-3">
+                    <button onclick="search();" type="submit" class="btn btn-search">
+                            SEARCH
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-
-    </div>
 </section>
-
-
-<!-- <div class="container">
-
-
-      
-    </div> -->
 
 <script type="text/javascript">
 $(document).on('ready', function() {

28882f039e3ba5aef08fddfced194755f3c1ef21
ittin2540 <fug2468man@gmail.com>
2019-03-08 17:39:25 +0700

    property detail

--- property.php
+++ property.php
@@ -49,6 +49,9 @@ $page = $page_model->getPageByID('4');
         <link href="template/frontend/css/menu.css" rel="stylesheet">
         <link href="template/frontend/css/footer.css" rel="stylesheet">
         <link href="template/frontend/css/home-style.css" rel="stylesheet">
+        <link href="template/frontend/css/service.css" rel="stylesheet">
+
+        
         <link href="template/frontend/css/property-types.css" rel="stylesheet">
         <link href="template/frontend/css/property-product.css" rel="stylesheet">
         <link href="template/frontend/css/property-detail.css" rel="stylesheet">
@@ -57,8 +60,8 @@ $page = $page_model->getPageByID('4');
         <?PHP require_once('view/menu.inc.php');?>
         <?PHP require_once('view/property/slide.inc.php');?>
         
-        <?PHP //require_once('view/property/property_type.inc.php');?>
-        <?PHP //require_once('view/property/property_product.inc.php');?>
+        <?PHP require_once('view/property/property_type.inc.php');?>
+        <?PHP require_once('view/property/property_product.inc.php');?>
         <?PHP require_once('view/property/property_detail.inc.php');?>
         
 

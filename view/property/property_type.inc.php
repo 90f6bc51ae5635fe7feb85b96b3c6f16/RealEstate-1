d63ac573eb6c42641974a459df871a2872b0ef2c
chunwarayut <warayut2555@gmail.com>
2019-03-11 10:06:03 +0700

    Update property_type.inc.php

--- view/property/property_type.inc.php
+++ view/property/property_type.inc.php
@@ -1,18 +1,19 @@
 <div class="top" id="property">
     <div class="col-12 p-0">
         <!-- div col-12 -->
-            <div class="text-center">
-                <h1 style="color: #5a5a5a; font-weight: 600">PROPERTY TYPES</h1>
-                <hr class="border-contact" style="border-top: 2px solid  #5a5a5a; width: 5%;">
-            </div>            
-            <div class="row m-0">
-                <?PHP for ($i=0; $i < count($type); $i++) { 
+        <div class="text-center">
+            <h1 style="color: #5a5a5a; font-weight: 600">PROPERTY TYPES</h1>
+            <hr class="border-contact" style="border-top: 2px solid  #5a5a5a; width: 5%;">
+        </div>
+        <div class="row m-0">
+            <?PHP for ($i=0; $i < count($type); $i++) { 
                     # code...
                     // $type[$i][''];
                     ?>
-                    <a href="property.php?action=detail&id=<?PHP echo $i; ?>">
-                <div class="col-lg-4 col-sm-6 col-md-4 p-1" style="position: relative;">
-                    <div class="proerty-bg" style="background-image: url(img_upload/types/<?PHP print_r($type[$i]['product_types_img']); ?>);">
+            <div class="col-lg-4 col-sm-6 col-md-4 p-1" style="position: relative;">
+                <a href="property.php?action=detail&id=<?PHP echo $i; ?>">
+                    <div class="proerty-bg"
+                        style="background-image: url(img_upload/types/<?PHP print_r($type[$i]['product_types_img']); ?>);">
                         <div class="property-img ">
                             <div class="property-content">
                                 <div class="property-h3">
@@ -21,10 +22,10 @@
                             </div>
                         </div>
                     </div>
-                </div>
                 </a>
-                <?php } ?>
             </div>
+            <?php } ?>
+        </div>
         <!-- div col-12 -->
     </div>
 </div>

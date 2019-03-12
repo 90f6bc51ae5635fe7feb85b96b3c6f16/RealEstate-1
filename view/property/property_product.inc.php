e3442760c87cbdc9b5d331e952eb139cd7c19724
chunwarayut <warayut2555@gmail.com>
2019-03-12 14:29:13 +0700

    Update property_product.inc.php

--- view/property/property_product.inc.php
+++ view/property/property_product.inc.php
@@ -2,22 +2,22 @@
     <div class="col-12 p-0">
         <!-- div col-12 -->
         <div class="text-center">
-                <h1 class="property-pro-h1">
-                    <?PHP if($lng == 'TH') {
-                        echo ($type_by_id['product_types_name_th']); 
-                    } else {  
-                        echo ($type_by_id['product_types_name_en']);  
-                    } ?>
-                </h1>
-                <hr class="border-contact" style="border-top: 2px solid  #5a5a5a; width: 5%;">
-            </div>            
+            <h1 class="property-pro-h1">
+                <?PHP if ($lng == 'TH') {
+                    echo ($type_by_id['product_types_name_th']);
+                } else {
+                    echo ($type_by_id['product_types_name_en']);
+                } ?>
+            </h1>
+            <hr class="border-contact" style="border-top: 2px solid  #5a5a5a; width: 5%;">
+        </div>
         <div class="row m-0">
             <!-- div row -->
-            <?PHP for ($i=0; $i < count($product_by_type); $i++) { 
-                    # code...
-                    // $type[$i][''];
-                    ?>
-            <div class="col-lg-4 col-sm-6 col-md-4 p-1 text-center" >
+            <?PHP for ($i = 0; $i < count($product_by_type); $i++) {
+                # code...
+                // $type[$i][''];
+                ?>
+            <div class="col-lg-4 col-sm-6 col-md-4 p-1 text-center">
                 <div style="background-color: #f2f2f2;">
                     <div class="col" style="padding: 0% 0% 5% 0%;">
                         <div class="property-bg" style="background-image: url(img_upload/product_image/<?PHP echo ($product_by_type[$i]['product_image_img']); ?>);">
@@ -25,34 +25,35 @@
                     </div>
                     <div class="col p-1">
                         <h4>
-                            <?PHP if($lng == 'TH') { 
-                                echo ($product_by_type[$i]['product_name_th']); 
+                            <?PHP if ($lng == 'TH') {
+                                echo ($product_by_type[$i]['product_name_th']);
                             } else {
-                                echo ($product_by_type[$i]['product_name_en']);  
+                                echo ($product_by_type[$i]['product_name_en']);
                             } ?>
                         </h4>
                     </div>
                     <div class="col-12" style="padding: 0% 1% 5% 1%;">
-                    <hr class="border-contact" style="border-top: 2px solid  #5a5a5a; width: 50%;">
+                        <hr class="border-contact" style="border-top: 2px solid  #5a5a5a; width: 50%;">
                         <p class="cut-text-multi">
-                        <?PHP if($lng == 'TH') { 
-                            echo ($product_by_type[$i]['product_detail_th']); 
-                        } else {  
-                            echo ($product_by_type[$i]['product_detail_en']);  
-                        } ?>
+                            <?PHP if ($lng == 'TH') {
+                                echo ($product_by_type[$i]['product_detail_th']);
+                            } else {
+                                echo ($product_by_type[$i]['product_detail_en']);
+                            } ?>
                         </p>
                     </div>
                     <div class="col" style="padding: 5% 0% 10% 0%;">
-                        <button type="button" class="btn property-pro-btn" tabindex="0">
+                        <a href="property.php?action=product&id=<?PHP echo $product_by_type[$i]['product_id']; ?>&type_id=<?PHP echo $product_by_type[$i]['product_types_id']; ?>" class="btn property-pro-btn" tabindex="0">
                             DETAIL >
-                        </button>
+                        </a>
                     </div>
                 </div>
             </div>
-            <?php } ?>
+            <?php 
+        } ?>
             <!-- /div row -->
         </div>
         <!-- /div col-12 -->
     </div>
     <!-- <> -->
-</div>
+</div> 

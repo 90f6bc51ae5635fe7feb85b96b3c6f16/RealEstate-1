ee5c33fb8d9d77f32f61dff0f1df82d9a6969ce2
boorapa <boorapa0147@gmail.com>
2019-03-12 09:16:52 +0700

    up

---
+++ view/news/newsdetail.inc.php
@@ -0,0 +1,37 @@
+
+<div class="row">
+        <div class="col-lg-2" >
+        </div>
+                
+                <div class="col-lg-8 no-padding" style="padding-top: 50px;">
+                    <div class="row">
+                        <img  src="img_upload/home/1.jpg" alt="Los Angles" class="img-newsdetail" >
+                    </div>
+                </div>
+
+    <div class="col-lg-2 ">
+    </div>
+</div>
+
+<div class="row" style="margin-bottom: 50px;">
+    <div class="col-lg-2 " >
+    </div>
+
+    <div class="col-lg-8 no-padding"  >
+        <div class="row news-detail">   
+            <div class="news-topic">
+           sdfsdfsdfsdf
+            </div>
+            <div  class="news-detail1">
+            sdfsdfsdf
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div class="col-lg-2 "  >
+        <div class="">
+            
+        </div>
+    </div>
+

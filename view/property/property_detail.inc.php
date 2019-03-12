35eb02d207689fb0a49eaa444cd1399675ccc54a
ittin2540 <fug2468man@gmail.com>
2019-03-12 10:29:42 +0700

    property-de-highlight

--- view/property/property_detail.inc.php
+++ view/property/property_detail.inc.php
@@ -1,26 +1,21 @@
 <div class="top">
     <div class="col-12 p-0 m-0">
         <!-- div col-12 -->
-
         <div class="row m-0">
             <!-- 1 -->
             <div class="col-lg-6 col-md-0 col-sm-0"></div>
             <div class="col-lg-6 col-md-12 col-sm-6">
                 <h1 class="proper ty-de-h1">
-
-
                     <?PHP if ($lng == 'TH') {
                         echo ($product[0]['product_name_th']);
                     } else {
                         echo ($product[0]['product_name_en']);
                     } ?>
-
                 </h1>
                 <hr class="border-contact" style="border-top: 2px solid  #5a5a5a; width:100%;">
             </div>
             <!-- 1 -->
         </div>
-
         <div class="row m-0" style="padding: 0% 0% 5% 0%;">
             <div class="col-lg-6 col-md-12 col-sm-6 p-0 ">
                 <section class="property-detail-img">
@@ -29,7 +24,6 @@
                     <?php 
                 } ?>
                 </section>
-
                 <script type="text/javascript">
                     $(document).on('ready', function() {
                         $(".property-detail-img").slick({
@@ -41,24 +35,18 @@
                         });
                     });
                 </script>
-
             </div>
-
             <div class="col-lg-6 col-md-12 col-sm-6">
                 <div class="row">
                     <div class="col" style="padding: 6% 0% 5% 7%;">
-
                         <?PHP if ($lng == 'TH') {
                             echo ($product[0]['product_detail_th']);
                         } else {
                             echo ($product[0]['product_detail_en']);
                         } ?>
-
-
                     </div>
                 </div>
                 <div>
-
                     <div class="row p-2 property-de-font p-0">
                         <div class="col-5 p-0">
                             Property Type
@@ -67,15 +55,11 @@
                             :
                         </div>
                         <div class="col-5 col-sm-4 p-0">
-
-
                             <?PHP if ($lng == 'TH') {
                                 echo ($type_by_id['product_types_name_th']);
                             } else {
                                 echo ($type_by_id['product_types_name_en']);
                             } ?>
-
-
                         </div>
                     </div>
                     <div class="row p-2">
@@ -86,14 +70,11 @@
                             :
                         </div>
                         <div class="col-5 col-sm-4 p-0">
-
-
                             <?PHP if ($lng == 'TH') {
                                 echo ($product[0]['product_name_th']);
                             } else {
                                 echo ($product[0]['product_name_en']);
                             } ?>
-
                         </div>
                     </div>
                     <div class="row p-2">
@@ -104,16 +85,11 @@
                             :
                         </div>
                         <div class="col-5 col-sm-4 p-0">
-
-
-
                             <?PHP if ($lng == 'TH') {
                                 echo ($product[0]['location_name_th']);
                             } else {
                                 echo ($product[0]['location_name_en']);
                             } ?>
-
-
                         </div>
                     </div>
                     <div class="row p-2">
@@ -124,20 +100,11 @@
                             :
                         </div>
                         <div class="col-5 col-sm-4 p-0">
-
-
-
-
-
                             <?PHP if ($lng == 'TH') {
                                 echo ($product[0]['product_bedroom']);
                             } else {
                                 echo ($product[0]['product_bedroom']);
                             } ?>
-
-
-
-
                         </div>
                     </div>
                     <div class="row p-2">
@@ -148,24 +115,11 @@
                             :
                         </div>
                         <div class="col-5 col-sm-4 p-0">
-
-
-
-
-
                             <?PHP if ($lng == 'TH') {
                                 echo ($product[0]['product_bathroom']);
                             } else {
                                 echo ($product[0]['product_bathroom']);
                             } ?>
-
-
-
-
-
-
-
-
                         </div>
                     </div>
                     <div class="row p-2">
@@ -176,24 +130,11 @@
                             :
                         </div>
                         <div class="col-5 col-sm-4 p-0">
-
-
-
-
-
                             <?PHP if ($lng == 'TH') {
                                 echo ($product[0]['furniture_name_th']);
                             } else {
                                 echo ($product[0]['furniture_name_en']);
                             } ?>
-
-
-
-
-
-
-
-
                         </div>
                     </div>
                     <div class="row p-2">
@@ -205,25 +146,11 @@
                         :
                     </div>
                     <div class="col-5 col-sm-4 p-0">
-
-
-
-
-
-
                         <?PHP if ($lng == 'TH') {
                             echo number_format($product[0]['product_price'], 2);
                         } else {
                             echo number_format($product[0]['product_price'], 2);
                         } ?>
-
-
-
-
-
-
-
-
                     </div>
                 </div>
 
@@ -238,59 +165,39 @@
                 </div>
             </div>
                 <div class="col-12" style="padding: 5% 0% 0% 0%">
-            <div class="row">
+            <div class="row m-0">
                     <div class="col-lg-6 col-md-12 col-sm-12 text-center" style="background-color: #f5f5f5;">
 
                         <div class="col-12" style="padding: 5% 0% 5% 0%">
                             HIGHLIGHT
                         </div>
-                        <div class="row">
+                        <div class="row m-0">
 
                             <?php for ($j = 0; $j < count($product_hightlight); ++$j) { ?>
                             <div class="col-lg-3 col-md-4 col-sm-6 p-2">
-                                <div class="col-12 property-bg" style="background-image: url(img_upload/product_highlight/<?PHP echo ($product_hightlight[$j]['product_highlight_img']); ?>);"></div>
-                                <div class="col-12">
-
-
-
-
-
-
+                                <div class="col-12 property-bg p-0" style="background-image: url(img_upload/product_highlight/<?PHP echo ($product_hightlight[$j]['product_highlight_img']); ?>);"></div>
+                                <div class="col-12 p-0">
                                         <?PHP if ($lng == 'TH') {
                                             echo ($product_hightlight[$j]['product_highlight_name_th']);
                                         } else {
                                             echo ($product_hightlight[$j]['product_highlight_name_en']);
                                         } ?>
-
-
-
-
-
-
-
-                                    <?PHP  ?>
                                 </div>
                             </div>
                             <?PHP 
                         } ?>
-
                         </div>
                     </div>
-                    <div class="col-lg-6 col-md-12 col-sm-12 text-center">
+                    <div class="col-lg-6 col-md-12 col-sm-12 text-center p-0">
                         <!-- Google Map -->
 
-
-
-
-                        <fieldset class="gllpLatlonPicker" style="margin: -14; height: auto;">
+                        <fieldset class="gllpLatlonPicker" style="height: auto;">
                             <div class="gllpMap" style="width: 100%;  height: 20%;">Google Maps</div>
                             <!-- <div class="gllpMap" style="width: 100%; height: 80%;">Google Maps</div> -->
                             <input type="text" class="gllpLatitude form-control" name="product_latitude" value="<?PHP echo $product[0]['product_latitude']; ?>" hidden />
                             <input type="text" class="gllpLongitude form-control" name="product_longitude" value="<?PHP echo $product[0]['product_longitude']; ?>" hidden />
                             <input type="hidden" class="gllpZoom" value="14" />
                         </fieldset>
-
-
 
                         <!-- /Google Map -->
                     </div>

18da77321f3b647540e3b2e54c707113112bbab1
boorapa <boorapa0147@gmail.com>
2019-03-12 09:46:18 +0700

    up

--- view/news/newsdetail.inc.php
+++ view/news/newsdetail.inc.php
@@ -1,37 +1,15 @@
+<div class="container">
+    <img src="img_upload/home/2.jpg" alt="Los Angles" class="img-newsdetail">
 
-<div class="row">
-        <div class="col-lg-2" >
+    <div class="row ">
+        <div class="news-topic">
+            sdfsdfsdfsdf
         </div>
-                
-                <div class="col-lg-8 no-padding" style="padding-top: 50px;">
-                    <div class="row">
-                        <img  src="img_upload/home/1.jpg" alt="Los Angles" class="img-newsdetail" >
-                    </div>
-                </div>
-
-    <div class="col-lg-2 ">
+    </div>
+    <div class="news-detail1">
+        sdfsdfsdfsdfsdfsdfsdfsfssghkluilfgfgnmhj.hfdfbvcbgwfdfsdfsdfasdasdasdczxczxczvcxvxvxcvxcvxcvfdgrtuipip[ouytgfsds]
+        sdfsdfsdfsddfsdfsdfsdfsdfsfssghkluilfgfgnmhj.hfdfbvcbgwfdfsdfsdfasdasdasdczxczxczvcxvxvxcvxcvx
+        dfsdfsdfsdfsdfsfssghkluilfgfgnmhj.hfdfbvcbgwfdfsdfsdfasdasdasdczxczxczvcxvxvxcvxcvx
+        dfsdfsdfsdfsdfsfssghkluilfgfgnmhj.hfdfbvcbgwfdfsdfsdfasdasdasdczxczxczvcxvxvxcvxcvx
     </div>
 </div>
-
-<div class="row" style="margin-bottom: 50px;">
-    <div class="col-lg-2 " >
-    </div>
-
-    <div class="col-lg-8 no-padding"  >
-        <div class="row news-detail">   
-            <div class="news-topic">
-           sdfsdfsdfsdf
-            </div>
-            <div  class="news-detail1">
-            sdfsdfsdf
-            </div>
-        </div>
-    </div>
-</div>
-
-    <div class="col-lg-2 "  >
-        <div class="">
-            
-        </div>
-    </div>
-

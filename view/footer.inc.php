35f2d31804117a29d961addf54291e343a8623ee
chunwarayut <warayut2555@gmail.com>
2019-03-13 11:51:09 +0700

    footer search

--- view/footer.inc.php
+++ view/footer.inc.php
@@ -1,28 +1,50 @@
 <?PHP 
-require_once('models/AboutUsModel.php');    
-$about_model = new AboutUsModel;   
-$about = $about_model-> getAbout_us(); 
+require_once('models/AboutUsModel.php');
+$about_model = new AboutUsModel;
+$about = $about_model->getAbout_us();
 $about = $about[0];
 
-require_once('models/TypesModel.php');    
-$type_model = new TypesModel;   
-$type = $type_model->  getTypesBy(); 
+require_once('models/TypesModel.php');
+$type_model = new TypesModel;
+$type = $type_model->getTypesBy();
 // print_r($type) ;
 
-require_once('models/LocationModel.php');    
-$location_model = new LocationModel;   
-$location = $location_model->  getLocationBy(); 
+require_once('models/LocationModel.php');
+$location_model = new LocationModel;
+$location = $location_model->getLocationBy();
 // print_r($location) ;
 
 
-require_once('models/NewsModel.php');    
-$news_model = new NewsModel;   
-$news = $news_model->  getNewsByDESC(); 
+require_once('models/NewsModel.php');
+$news_model = new NewsModel;
+$news = $news_model->getNewsByDESC();
 // print_r($news) ;
 
 
 
 ?>
+
+<script>
+    function search_product_types(id) {
+        // var location_id = $("#location_id").val();
+        var product_types_id = document.getElementById("product_types_ids_" + id).value;
+        // console.log(product_types_id);
+
+        // var keyword = $("#keyword").val();
+        // window.location = "property.php?action=detail&search=1&location_id=" + location_id + "&product_types_id=" + product_types_ids + "&keyword=" + keyword;
+        window.location = "property.php?action=detail&search=1&product_types_id=" + product_types_id;
+    }
+
+    function search_location(id) {
+        // var location_id = $("#location_id").val();
+        var location_id = document.getElementById("search_location_" + id).value;
+        // console.log(product_types_id);
+
+        // var keyword = $("#keyword").val();
+        // window.location = "property.php?action=detail&search=1&location_id=" + location_id + "&product_types_id=" + product_types_ids + "&keyword=" + keyword;
+        window.location = "property.php?action=detail&search=1&location_id=" + location_id;
+    }
+</script>
 
 <footer>
     <div class="container con-footer">
@@ -34,19 +56,19 @@ $news = $news_model->  getNewsByDESC();
                 </div>
                 <br>
                 <div style="font-size: 1rem;">
-                    <?PHP if ($lan=="TH") {
-                    echo $about['about_us_title_th'];
-                } else{
-                    echo $about['about_us_title_en'];
-                }?>
+                    <?PHP if ($lan == "TH") {
+                        echo $about['about_us_title_th'];
+                    } else {
+                        echo $about['about_us_title_en'];
+                    } ?>
                 </div>
                 <br>
                 <div style="font-size: 1rem;">
-                    <?PHP if ($lan=="TH") {
-                    echo $about['about_us_sub_title_th'];
-                } else{
-                    echo $about['about_us_sub_title_en'];
-                }?>
+                    <?PHP if ($lan == "TH") {
+                        echo $about['about_us_sub_title_th'];
+                    } else {
+                        echo $about['about_us_sub_title_en'];
+                    } ?>
                 </div>
             </div>
             <div class="col-md-5 footer-panel">
@@ -56,15 +78,19 @@ $news = $news_model->  getNewsByDESC();
                             PROPERTY TYPE
                         </div>
                         <div style="font-size: 0.95rem;">
-                            <?PHP for ($i=0; $i < count($type); $i++) {  ?>
+                            <?PHP for ($i = 0; $i < count($type); $i++) {  ?>
                             <i class="fas fa-angle-right"></i>
-                            <?PHP if ($lan=="TH") {
-                            echo $type[$i]['product_types_name_th'];
-                        } else{
-                            echo $type[$i]['product_types_name_en'];
-                        }?>
+                            <input type="hidden" id="product_types_ids_<?php echo $i; ?>" name="product_types_ids" value="<?php echo $type[$i]['product_types_id']; ?>">
+                            <button onclick="search_product_types('<?php echo $i; ?>');">
+                                <?PHP if ($lan == "TH") {
+                                    echo $type[$i]['product_types_name_th'];
+                                } else {
+                                    echo $type[$i]['product_types_name_en'];
+                                } ?>
+                            </button>
                             <br>
-                            <?PHP } ?>
+                            <?PHP 
+                        } ?>
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -72,15 +98,19 @@ $news = $news_model->  getNewsByDESC();
                             LOCATIONS
                         </div>
                         <div style="font-size: 0.95rem;">
-                            <?PHP for ($i=0; $i < count($location); $i++) {  ?>
-                            <i class="fas fa-angle-right"></i>
-                            <?PHP if ($lan=="TH") {
-                            echo $location[$i]['location_name_th'];
-                        } else{
-                            echo $location[$i]['location_name_en'];
-                        }?>
+                            <?PHP for ($i = 0; $i < count($location); $i++) {  ?>
+                            <input type="hidden" id="search_location_<?php echo $i; ?>" name="search_location" value="<?php echo $location[$i]['location_id']; ?>">
+                            <button onclick="search_location('<?php echo $i; ?>');">
+                                <i class="fas fa-angle-right"></i>
+                                <?PHP if ($lan == "TH") {
+                                    echo $location[$i]['location_name_th'];
+                                } else {
+                                    echo $location[$i]['location_name_en'];
+                                } ?>
+                            </button>
                             <br>
-                            <?PHP } ?>
+                            <?PHP 
+                        } ?>
                         </div>
                     </div>
                 </div>
@@ -92,31 +122,31 @@ $news = $news_model->  getNewsByDESC();
                 <div>
 
                     <div class="col-12 ">
-                        <?PHP for ($i=0; $i < 2 ; $i++) { 
-                    # code...
-                // print_r(count ($news));?>
+                        <?PHP for ($i = 0; $i < 2; $i++) {
+                            # code...
+                            ?>
                         <div class="row">
                             <div class="col-4 col-img-footer">
-                                <img src="img_upload/news/<?PHP echo $news[$i]['news_img']; ?>"
-                                    class="rounded float-left img-fluid img-size-footer " alt="...">
+                                <img src="img_upload/news/<?PHP echo $news[$i]['news_img']; ?>" class="rounded float-left img-fluid img-size-footer " alt="...">
                             </div>
                             <div class="col-8">
                                 <?PHP if ($lan == 'TH') {
-                                # code...
-                                echo $news[$i]['news_name_th'];
-                            } else {
-                               echo $news[$i]['news_name_en']; 
-                            }  ?>
+                                    # code...
+                                    echo $news[$i]['news_name_th'];
+                                } else {
+                                    echo $news[$i]['news_name_en'];
+                                }  ?>
                                 <br>
                                 <p class="light "><i class="far fa-clock light"></i>
-                                    <?PHP echo  date("M d, Y ",strtotime($news[$i]['news_date']));?>
+                                    <?PHP echo  date("M d, Y ", strtotime($news[$i]['news_date'])); ?>
                                 </p>
 
 
                             </div>
                         </div>
                         <br>
-                        <?PHP } ?>
+                        <?PHP 
+                    } ?>
                     </div>
 
                 </div>
@@ -130,17 +160,17 @@ $news = $news_model->  getNewsByDESC();
 
 
 <style>
-.col-img-footer {
-    padding-right: 5;
-    padding-left: 0;
-}
+    .col-img-footer {
+        padding-right: 5;
+        padding-left: 0;
+    }
 
-.img-size-footer {
-    max-width: 100%;
-    height: 60;
-}
+    .img-size-footer {
+        max-width: 100%;
+        height: 60;
+    }
 
-.light {
-    color: #0009;
-}
-</style>
+    .light {
+        color: #0009;
+    }
+</style> 

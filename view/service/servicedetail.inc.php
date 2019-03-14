43dd344440c599adf7255a67df864d1e9e04533c
boorapa <boorapa0147@gmail.com>
2019-03-14 10:23:21 +0700

    up

--- view/service/servicedetail.inc.php
+++ view/service/servicedetail.inc.php
@@ -5,18 +5,18 @@
     </div>
     <?PHP $i = $_GET['id'] ; ?>
     <div class="row shadow-lg">
-        <div class="col-lg-6 col-md-6 col-sm-6 col-6 no-padding">
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12 no-padding">
             <img class="img-service-detail" src="img_upload/services/<?PHP  echo $services[$i]['services_img']; ?>">
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-6 col-6 no-padding">
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12 no-padding">
 
             <div class="about-head">
                 <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-4 no-padding">
+                    <div class="col-lg-4 col-md-3 col-sm-3 col-3 no-padding">
                         <img class="icon-service-detail"
                             src="img_upload/icon-services/53176142_310652063137173_1974414113991819264_n.png">
                     </div>
-                    <div class="col-lg-4 col-md-8 col-sm-8 col-8 all-text-service-detail no-padding">
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-6 all-text-service-detail no-padding">
                         <div class="text-sevice-detail1">
                             <?PHP if($lng == "TH"){ echo 'บริการสำหรับ'; }else{ echo ' SERVICE FOR'; } ?>
                             <br>
@@ -24,7 +24,7 @@
 
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-8 col-sm-8 col-8 no-padding">
+                    <div class="col-lg-4 col-md-3 col-sm-3 col-3 no-padding">
                         <a href="contact.php#contact" class="btn btn-contact-servicedetail">
                             CONTACT US
                         </a>

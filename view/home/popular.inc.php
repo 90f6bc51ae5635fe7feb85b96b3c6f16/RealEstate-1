d7c5b6a1ffafc2496eb638dd24861be333cff6b7
ittin2540 <fug2468man@gmail.com>
2019-03-14 16:31:28 +0700

    zczc

--- view/home/popular.inc.php
+++ view/home/popular.inc.php
@@ -1,27 +1,31 @@
 <div class="container" style="margin-bottom: 7vw;">
     <fieldset class="border-poplar-home ">
-        <legend class = "topic-popular-home">
+        <legend class="topic-popular-home">
             POPPULAR PROPERTIES
         </legend>
         <div class="row" style="max-width: 1100px;margin: auto;">
             <div class="element-item col-lg-6 col-md-6 col-sm-6 col-6 img-poppular-home-padding">
-                <img class="img-poppular-home1" src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
+                <img class="img-poppular-home1"
+                    src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
             </div>
             <div class="element-item col-lg-6 col-md-6 col-sm-6 col-6 img-poppular-home-padding">
-                <img class="img-poppular-home1" src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
+                <img class="img-poppular-home1"
+                    src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
             </div>
         </div>
         <div class="row" style="max-width: 1100px;margin: auto; padding-top: 1vw; padding-bottom: 1vw;">
             <div class="element-item col-lg-4 col-md-4 col-sm-4 col-4 img-poppular-home-padding">
-                <img class="img-poppular-home2" src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
+                <img class="img-poppular-home2"
+                    src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
             </div>
             <div class="element-item col-lg-4 col-md-4 col-sm-4 col-4 img-poppular-home-padding">
-                <img class="img-poppular-home2" src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
+                <img class="img-poppular-home2"
+                    src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
             </div>
             <div class="element-item col-lg-4 col-md-4 col-sm-4 col-4 img-poppular-home-padding">
-                <img class="img-poppular-home3" src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
+                <img class="img-poppular-home3"
+                    src="img_upload/types/<?PHP  echo ($type_popular[0]['product_types_img']) ?>">
             </div>
         </div>
     </fieldset>
-</div>
 </div>

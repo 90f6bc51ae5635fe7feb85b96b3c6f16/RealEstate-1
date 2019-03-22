2a6520ea76b7f0ecec3c59c5ee4148aace3f5652
ittin2540 <45927706+ittin2540@users.noreply.github.com>
2019-03-22 12:10:09 +0700

    sike

--- view/home/product.inc.php
+++ view/home/product.inc.php
@@ -1,73 +1,63 @@
-
 <div style="padding: 5% 10%;">
-    <div class="text-center margin-product-home">
-        <div class="text-product-home-topic">
-        <?PHP if ($lng == 'TH') { ?>
-           <?PHP  echo ($product_header[0]['product_header_th']) ?>
-        <?PHP } else { ?>
-           <?PHP  echo ($product_header[0]['product_header_en']) ?>
-        <?PHP } ?>
-        </div>
-        <div class="text-product-home cut-text-multi-product-home">
-        
-        <?PHP if ($lng == 'TH') { ?>
-           <?PHP  echo ($product_header[0]['product_header_detail_th']) ?>
-        <?PHP } else { ?>
-           <?PHP  echo ($product_header[0]['product_header_detail_en']) ?>
-        <?PHP } ?>
+   <div class="text-center margin-product-home">
+      <div class="text-product-home-topic">
+         <?PHP if ($lng == 'TH') { ?>
+         <?PHP  echo ($product_header[0]['product_header_th']) ?>
+         <?PHP } else { ?>
+         <?PHP  echo ($product_header[0]['product_header_en']) ?>
+         <?PHP } ?>
+      </div>
+      <div class="text-product-home cut-text-multi-product-home">
+         <?PHP if ($lng == 'TH') { ?>
+         <?PHP  echo ($product_header[0]['product_header_detail_th']) ?>
+         <?PHP } else { ?>
+         <?PHP  echo ($product_header[0]['product_header_detail_en']) ?>
+         <?PHP } ?>
+      </div>
 
+      <div class="responsive">
+            <?PHP 
+                for($i=0;$i<count($product );$i++){ 
+            ?>
+         <div class="" style="border: ridge;">
+            <img class="size-img-product-home"
+               src="img_upload/product_image/<?PHP echo $product[$i]['product_image_img'];?>" alt="">
+            <div class="text-regular" style="padding-top: 20px;">
+               <div class="product-product-name">
+                  <?PHP if ($lng == 'TH') { ?>
+                  <?PHP  echo ($product[$i]['product_name_th']) ?>
+                  <?PHP } else { ?>
+                  <?PHP  echo ($product[$i]['product_name_en']) ?>
+                  <?PHP } ?>
+               </div>
+               <div class="product-product-detail cut-text-multi ">
 
-        </div>
-        
-        <div class="responsive" style="margin-right: 1vw;">
-                <?PHP for($i=0;$i<count($product );$i++){ ?>
-                <div class="" style="border: ridge;">
-                    <img class="size-img-product-home" src="img_upload/product_image/<?PHP echo $product[$i]['product_image_img'];?>"
-                        alt="">
-                    <div class="text-regular" style="padding-top: 20px;">
-                        <div class="product-product-name">
-
-
-                            <?PHP if ($lng == 'TH') { ?>
-                            <?PHP  echo ($product[$i]['product_name_th']) ?>
-                            <?PHP } else { ?>
-                            <?PHP  echo ($product[$i]['product_name_en']) ?>
-                            <?PHP } ?>
-
-
-                        </div>
-                        <div class="product-product-detail cut-text-multi ">
-
-                            <?PHP if ($lng == 'TH') { ?>
-                            <?PHP  echo ($product[$i]['product_detail_th']) ?>
-                            <?PHP } else { ?>
-                            <?PHP  echo ($product[$i]['product_detail_en']) ?>
-                            <?PHP } ?>
-
-
-                        </div>
-                    </div>
-                    <div class="text-regular" style="padding: 16px 0px; color: #c6c6c6;">
-                    <a href="property.php?action=product&id=<?PHP echo $product[$i]['product_id']; ?>&type_id=<?PHP echo $product[$i]['product_types_id']; ?>" class="btn btn-outline-success" tabindex="0">
-                    <?PHP
-                        if($lng == "TH"){
-                            echo 'รายละเอียด';
-                        }else{
-                            echo 'VIEW DETAIL';
-                        } 
-                        ?>
-                    </a>
-                        <button class="btn btn-primary"
-                            type="submit"><?php echo number_format($product[$i]['product_price'],2); ?></button>
-                    </div>
-                </div>
-                <?PHP } ?>
-           
-        </div>
-    </div>
+                  <?PHP if ($lng == 'TH') { ?>
+                  <?PHP  echo ($product[$i]['product_detail_th']) ?>
+                  <?PHP } else { ?>
+                  <?PHP  echo ($product[$i]['product_detail_en']) ?>
+                  <?PHP } ?>
+               </div>
+            </div>
+            <div class="text-regular" style="padding: 16px 0px; color: #c6c6c6;">
+               <a href="property.php?action=product&id=<?PHP echo $product[$i]['product_id']; ?>&type_id=<?PHP echo $product[$i]['product_types_id']; ?>"
+                  class="btn btn-outline-success" tabindex="0">
+                  <?PHP
+if($lng == "TH"){
+echo 'รายละเอียด';
+}else{
+echo 'VIEW DETAIL';
+} 
+?>
+               </a>
+               <button class="btn btn-primary"
+                  type="submit"><?php echo number_format($product[$i]['product_price'],2); ?></button>
+            </div>
+         </div>
+         <?PHP } ?>
+      </div>
+   </div>
 </div>
-
-
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=K2D:300');
@@ -75,42 +65,41 @@
 
 <script>
 $(document).on('ready', function() {
-    $('.responsive').slick({
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        speed: 200,
-        slidesToShow: 3,
-        arrows: false,
-        slidesToScroll: 3,
-        responsive: [
-            {
+   $('.responsive').slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 200,
+      slidesToShow: 3,
+      arrows: false,
+      slidesToScroll: 3,
+      responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+               slidesToShow: 3,
+               slidesToScroll: 3,
+               infinite: true,
+               dots: true
             }
-            },
-            {
+         },
+         {
             breakpoint: 770,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+               slidesToShow: 2,
+               slidesToScroll: 2
             }
-            },
-            {
+         },
+         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+               slidesToShow: 1,
+               slidesToScroll: 1
             }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-});
+         }
+         // You can unslick at a given breakpoint now by adding:
+         // settings: "unslick"
+         // instead of a settings object
+      ]
+   });
 });
 </script>

189ca67e210a0cace25ec0e22ff2868f101e073a
ittin2540 <45927706+ittin2540@users.noreply.github.com>
2019-03-25 11:24:19 +0700

    .....

--- property.php
+++ property.php
@@ -61,66 +61,53 @@ $product_header = $product_model->getProductHeaderBy();
 <head>
 
 
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="<?PHP if ($lng == " TH") {
-                                            echo $page['page_title_th'];
-                                        } else {
-                                            echo $page['page_title_en'];
-                                        } ?>">
-    <meta property="og:description" content="<?PHP if ($lng == " TH") {
-                                                    echo $page['page_description_th'];
-                                                } else {
-                                                    echo $page['page_description_en'];
-                                                } ?>">
+   <meta property="og:type" content="website">
+   <meta property="og:title" content="<?PHP if ($lng == " TH") { echo $page['page_title_th']; } else { echo
+      $page['page_title_en']; } ?>">
+   <meta property="og:description" content="<?PHP if ($lng == " TH") { echo $page['page_description_th']; } else { echo
+      $page['page_description_en']; } ?>">
 
 
 
 
 
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="utf-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>REAL ESTATE PROPERTY PAGE </title>
+   <title>REAL ESTATE PROPERTY PAGE </title>
 
-    <script src="template/js/jquery-2.2.0.min.js" type="text/javascript"></script>
+   <script src="template/js/jquery-2.2.0.min.js" type="text/javascript"></script>
 
-    <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" type="text/css" media="screen" href="template/bootstrap/css/bootstrap.min.css" />
-    <script src="template/bootstrap/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+   <!-- Bootstrap Core CSS -->
+   <link rel="stylesheet" type="text/css" media="screen" href="template/bootstrap/css/bootstrap.min.css" />
+   <script src="template/bootstrap/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-    <!-- slide -->
-    <link rel="stylesheet" type="text/css" href="template/slick/slick.css">
-    <script src="template/slick/slick.min.js" type="text/javascript" charset="utf-8"></script>
+   <!-- slide -->
+   <link rel="stylesheet" type="text/css" href="template/slick/slick.css">
+   <script src="template/slick/slick.min.js" type="text/javascript" charset="utf-8"></script>
 
-    <!-- Custom Fonts & CSS -->
-    <link rel="stylesheet" type="text/css" media="screen" href="template/css/style.css" />
+   <!-- Custom Fonts & CSS -->
+   <link rel="stylesheet" type="text/css" media="screen" href="template/css/style.css" />
 
-    <link href="template/frontend/css/menu.css" rel="stylesheet">
-    <link href="template/frontend/css/footer.css" rel="stylesheet">
-    <link href="template/frontend/css/home-style.css" rel="stylesheet">
-    <link href="template/frontend/css/service.css" rel="stylesheet">
+   <link href="template/frontend/css/menu.css" rel="stylesheet">
+   <link href="template/frontend/css/footer.css" rel="stylesheet">
+   <link href="template/frontend/css/home-style.css" rel="stylesheet">
+   <link href="template/frontend/css/service.css" rel="stylesheet">
 
 
-    <link href="template/frontend/css/property-types.css" rel="stylesheet">
-    <link href="template/frontend/css/property-product.css" rel="stylesheet">
-    <link href="template/frontend/css/property-detail.css" rel="stylesheet">
+   <link href="template/frontend/css/property-types.css" rel="stylesheet">
+   <link href="template/frontend/css/property-product.css" rel="stylesheet">
+   <link href="template/frontend/css/property-detail.css" rel="stylesheet">
 </head>
 
 <body>
-    <?PHP require_once('view/menu.inc.php'); ?>
-    <?PHP require_once('view/slide.inc.php'); ?>
-
-
-
-    <?PHP
-
-
-
-
-
+   <?PHP require_once('view/menu.inc.php'); ?>
+   <?PHP require_once('view/slide.inc.php'); ?>
+   <?PHP
     if (!isset($_GET['action'])) {
 
         require_once('view/property/property_type.inc.php');
@@ -134,16 +121,7 @@ $product_header = $product_model->getProductHeaderBy();
         # code...
     }
     ?>
-
-
-
-
-
-
-
-
-
-    <?PHP require_once('view/footer.inc.php'); ?>
+   <?PHP require_once('view/footer.inc.php'); ?>
 </body>
 
-</htm l>
+</html>
